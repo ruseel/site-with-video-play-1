48ecdc03e6e1d6b63567a332ba1e12e51148e5e8
r <r@rsl.kr>
2025-08-20 15:22:19 +0900

    expose axd.nav("..url..")

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 import Agent1Waiting from './components/Agent1Waiting'
 import Agent1Video from './components/Agent1Video'
 import Agent1Acting from './components/Agent1Acting'
 import io from 'socket.io-client'
 
-// Worker setup in global scope
-const tunnelId = 'tunnel-' + Math.random().toString(36).substr(2, 9);
+// js-eval-tunnel setup in global scope
+const tunnelId = 'tunnel-agent1';
 const socket = io('http://localhost:3000');
 
 socket.on('connect', () => {
@@ -64,15 +66,20 @@ window.workerInfo = {
 };
 
 function App() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    window.axd = {
+        "nav": navigate
+    }
+  }, [navigate]);
+
   return (
-    <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/agent1/waiting" replace />} />
         <Route path="/agent1/waiting" element={<Agent1Waiting />} />
         <Route path="/agent1/video1" element={<Agent1Video />} />
         <Route path="/agent1/acting" element={<Agent1Acting />} />
       </Routes>
-    </Router>
   )
 }
 
